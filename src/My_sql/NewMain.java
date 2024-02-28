0e9f8c68a2814047a28d7c82d66f21849702fc04
zedl3all <sanguanwongtanapat@gmail.com>
2024-02-28 21:32:33 +0700

    Change code to test file CheckLogin_Data.java.

--- src/My_sql/NewMain.java
+++ src/My_sql/NewMain.java
@@ -4,7 +4,7 @@ package My_sql;
 public class NewMain{
 
     public static void main(String[] args) {
-        AddLogin_Data p1 = new AddLogin_Data("sys", "users");
-        p1.insertData("zedl3all","66070082@kmitl.ac.th", "1234", "Tanapat", "Sanguanwong");
+        CheckLogin_Data p1 = new CheckLogin_Data("sys", "users");
+        System.out.println(p1.CheckData("Pao", "456"));
     }
 }
