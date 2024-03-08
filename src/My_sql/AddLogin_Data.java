54a5208da290a26dd27c5ab0ff992695b3411448
zedl3all <sanguanwongtanapat@gmail.com>
2024-03-09 02:45:24 +0700

    Change code for new sql and check 2 step for already have data.

--- src/My_sql/AddLogin_Data.java
+++ src/My_sql/AddLogin_Data.java
@@ -4,38 +4,49 @@ import java.sql.*;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class AddLogin_Data extends My_sql{
-    public AddLogin_Data() {
-        super();
-    }
-
-    public AddLogin_Data(String schema, String table) {
-        super(schema, table);
-    }
+    
+    CheckRegis_Data checkdata = new CheckRegis_Data();
 
     public void insertData(String username, String email, String password, String firstname, String lastname){
-        try {
-            super.connect();
-            Connection conn = super.get_Connection();
-            
-            try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO " + super.get_Table() + " (username, email, password, firstname, lastname) VALUES (?, ?, ?, ?, ?)")) {
-                pstmt.setString(1, username);
-                pstmt.setString(2, email);
-                pstmt.setString(3, BCrypt.hashpw(password, BCrypt.gensalt()));
-                pstmt.setString(4, firstname);
-                pstmt.setString(5, lastname);
+        if (checkdata.CheckEmail(email)|checkdata.CheckUsername(username)){
+            System.out.println("Can't add data because it's already have data.");
+        }else{
+            try {
+                super.set_Schema("staff");
                 
-                pstmt.executeUpdate();
-
-                System.out.println("Add data completed.");
+                super.connect();
+                Connection conn = super.get_Connection();
                 
-                System.out.println("Disconnect PreparedStatement.");
+                try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO " + "staff_info" + " (first_name, last_name, email) VALUES (?, ?, ?)")) {
+                
+                    pstmt.setString(1, firstname);
+                    pstmt.setString(2, lastname);
+                    pstmt.setString(3, email);
+                
+                    pstmt.executeUpdate();
+                    
+                    System.out.println("Add data 1 completed.");
+                    
+                }
+                try(PreparedStatement pstmt = conn.prepareStatement("INSERT INTO " + "staff_login" + " (staff_user, staff_pass) VALUES (?, ?)")){
+                    pstmt.setString(1, username);
+                    pstmt.setString(2, BCrypt.hashpw(password, BCrypt.gensalt()));
+                    
+                    pstmt.executeUpdate();
+                    
+                    System.out.println("Add data 2 completed.");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }finally{
+                super.disconnect();
             }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally{
-            super.disconnect();
         }
-            
+    }
+        
+    
+    public static void main(String[] args) {
+        AddLogin_Data t1 = new AddLogin_Data();
+        t1.insertData("zedl3all","66070082@kmitl.ac.th","1234","Tanapat","Sanguanwong");
     }
 }
