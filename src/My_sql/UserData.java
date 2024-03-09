32caadcd1ecea5e2f74e245b7db29eda0376ba76
zedl3all <sanguanwongtanapat@gmail.com>
2024-03-10 01:18:24 +0700

    Create abstract Class

---
+++ src/My_sql/UserData.java
@@ -0,0 +1,12 @@
+package My_sql;
+
+public abstract class UserData extends My_sql{
+    
+    public abstract void InsertData(String username, String email, String password, String firstname, String lastname);
+    public abstract boolean CheckLogin(String username, String password);
+    public abstract boolean CheckUsername(String username);
+    public abstract boolean CheckEmail(String email);
+    public abstract void ChangeUsername(String oldusername, String newusername);
+    public abstract void ChangePassword(String username, String password);
+    
+}
