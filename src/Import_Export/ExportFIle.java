499b530deee5ea73d00a7e57aee1d72fa4d35db5
Panot Liangpiboon <138891084+iPaonN@users.noreply.github.com>
2024-03-27 21:33:14 +0700

    แก้ชื่อ
    แก้ให้โต้
    
    Co-Authored-By: Thanapolnc <138879665+Thanapolnc@users.noreply.github.com>

--- src/Import_Export/ExportFIle.java
+++ src/Import_Export/ExportFIle.java
@@ -4,56 +4,76 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.*;
 
-public class ExportFIle {
+public class ExportFile {
     public JFileChooser fileChooser;
-    public int val;
-    public File choose;
+    public int check;
     
     
-    public void Myexport(File importfile){
+    public void Myexport(String File_Extension, File importfile){
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         try {
             fileChooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter(File_Extension.toUpperCase()+" Files", File_Extension);
+            fileChooser.setFileFilter(filter);
             fileChooser.setDialogTitle("Save As");
-            fileChooser.setSelectedFile(new File(""));
-            val = fileChooser.showSaveDialog(null);
-            if (val == JFileChooser.APPROVE_OPTION) {
+            fileChooser.setSelectedFile(new File("UNITED."+File_Extension));
+            fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+            check = fileChooser.showSaveDialog(null);
+            if (check == JFileChooser.APPROVE_OPTION) {
                 File fileToSave = fileChooser.getSelectedFile();
-                try (InputStream is = new FileInputStream(importfile); OutputStream os = new FileOutputStream(fileToSave)) {
+                try (InputStream is = new FileInputStream(importfile); 
+                        OutputStream os = new FileOutputStream(fileToSave)) {
                     byte[] buffer = new byte[1024];
                     int length;
-                    while ((length = is.read(buffer)) > 0) {
+                    while ((length = is.read(buffer)) != -1) {
                         os.write(buffer, 0, length);
                     }
                 }
-                
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            }   
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
     
-    public void Myexport(FileInputStream importfile){
+    
+    //For Path Directory
+    public void Myexport(String File_Extension, String path){
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         try {
             fileChooser = new JFileChooser();
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("XLSX Files", "xlsx");
+            FileNameExtensionFilter filter = new FileNameExtensionFilter(File_Extension.toUpperCase()+" Files", File_Extension);
             fileChooser.setFileFilter(filter);
             fileChooser.setDialogTitle("Save As");
-            fileChooser.setSelectedFile(new File("UNITED.xlsx"));
-            val = fileChooser.showSaveDialog(null);
-            if (val == JFileChooser.APPROVE_OPTION) {
+            fileChooser.setSelectedFile(new File("UNITED."+File_Extension));
+            fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+            check = fileChooser.showSaveDialog(null);
+            if (check == JFileChooser.APPROVE_OPTION) {
                 File fileToSave = fileChooser.getSelectedFile();
-                try (OutputStream os = new FileOutputStream(fileToSave)) {
+                try (FileInputStream importfile = new FileInputStream(path);
+                        OutputStream os = new FileOutputStream(fileToSave)) {
                     byte[] buffer = new byte[1024];
                     int length;
                     while ((length = importfile.read(buffer)) != -1) {
                         os.write(buffer, 0, length);
                     }
+                }catch (IOException ex) {
+                    ex.printStackTrace();
                 }
-                
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            }   
+        }catch(Exception e){
+            e.printStackTrace();
         }
-    }
+    }   
+
+
+
     
 }
