57b1852b9dc345a9f351a1836c84ff39565ee2c4
SAKURA <141387173+Stacla3@users.noreply.github.com>
2024-03-29 23:17:01 +0700

    History 100% UI(Delete ViewEditor and ViewRenderer) change(ViewHistory)

--- src/UI/View/ViewHistory.java
+++ src/UI/View/ViewHistory.java
@@ -10,13 +10,24 @@ public class ViewHistory {
     private JTable table;
     private JScrollPane scrollPane;
     private DefaultTableModel model;
-    private final Object[] columnName = {"Date", "Time", "Name", "Amount", "Add", "Remove"};
+    private final Object[] columnName = {"History_ID", "Product_ID", "Product_Name",
+        "Amount", "Type", "Action", "Date", "Time", "User"};
+    private JPanel top, empty1, empty2, empty3;
+    private JLabel header, date;
+    private DefaultTableCellRenderer renderer;
     
     public ViewHistory(){
         frame = new JFrame();
         table = new JTable();
         scrollPane = new JScrollPane();
         model = new DefaultTableModel();
+        top = new JPanel();
+        header = new JLabel("<html><font color='#4F5A6C'>HIS</font><font color='#4A649D'>TORY AT</font></html>");
+        date = new JLabel("3/3/2024");
+        empty1 = new JPanel();
+        empty2 = new JPanel();
+        empty3 = new JPanel();
+        renderer = new DefaultTableCellRenderer();
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setTitle("History");
         
@@ -25,36 +36,64 @@ public class ViewHistory {
         table.setFont(new Font("Arial", Font.BOLD, 16));
         model.setColumnIdentifiers(columnName);
         Object[][] dataRows = {
-            {"3/3/2024", "4:35 PM", "Cola", 500, true, false},
-            {"3/3/2024", "4:25 PM", "Lays", 10, false, true}
+            {"1", "101", "Coke", 100, "Drink", "Increase", "3/3/2024", "4:35 PM", "ZedBall"},
+            {"2", "102", "Banana", 50, "Fruit", "Decrease", "3/3/2024", "4:34 PM", "ZedBall"},
+            {"3", "103", "Pineapple", 30, "Fruit", "Remove", "3/3/2024", "4:33 PM", "ZedBall"}
         };
         for (Object[] dataRow : dataRows) {
             model.insertRow(model.getRowCount(), dataRow);
         }
+        renderer.setHorizontalAlignment(JLabel.CENTER);
         table.setDefaultEditor(Object.class, null);
         table.setCellSelectionEnabled(false);
         table.setRowHeight(50);
-        table.getColumnModel().getColumn(0).setPreferredWidth(200);
-        table.getColumnModel().getColumn(1).setPreferredWidth(200);
-        table.getColumnModel().getColumn(2).setPreferredWidth(300);
-        table.getColumnModel().getColumn(3).setPreferredWidth(200);
-        table.getColumnModel().getColumn(0).setCellRenderer(new ViewRenderer());
-        table.getColumnModel().getColumn(1).setCellRenderer(new ViewRenderer());
-        table.getColumnModel().getColumn(2).setCellRenderer(new ViewRenderer());
-        table.getColumnModel().getColumn(3).setCellRenderer(new ViewRenderer());
-        table.getColumnModel().getColumn(4).setCellRenderer(new ViewRenderer());
-        table.getColumnModel().getColumn(4).setCellEditor(new ViewEditor(new JCheckBox()));
-        table.getColumnModel().getColumn(4).setPreferredWidth(100);
-        table.getColumnModel().getColumn(5).setPreferredWidth(100);
-        table.getColumnModel().getColumn(5).setCellRenderer(new ViewRenderer());
-        table.getColumnModel().getColumn(5).setCellEditor(new ViewEditor(new JCheckBox()));
+        table.getColumnModel().getColumn(0).setPreferredWidth(150);
+        table.getColumnModel().getColumn(1).setPreferredWidth(150);
+        table.getColumnModel().getColumn(2).setPreferredWidth(250);
+        table.getColumnModel().getColumn(3).setPreferredWidth(150);
+        table.getColumnModel().getColumn(4).setPreferredWidth(200);
+        table.getColumnModel().getColumn(5).setPreferredWidth(200);
+        table.getColumnModel().getColumn(6).setPreferredWidth(200);
+        table.getColumnModel().getColumn(7).setPreferredWidth(200);
+        table.getColumnModel().getColumn(8).setPreferredWidth(200);
+        table.getColumnModel().getColumn(0).setCellRenderer(renderer);
+        table.getColumnModel().getColumn(1).setCellRenderer(renderer);
+        table.getColumnModel().getColumn(2).setCellRenderer(renderer);
+        table.getColumnModel().getColumn(3).setCellRenderer(renderer);
+        table.getColumnModel().getColumn(4).setCellRenderer(renderer);
+        table.getColumnModel().getColumn(5).setCellRenderer(renderer);
+        table.getColumnModel().getColumn(6).setCellRenderer(renderer);
+        table.getColumnModel().getColumn(7).setCellRenderer(renderer);
+        table.getColumnModel().getColumn(8).setCellRenderer(renderer);
+        table.getTableHeader().setForeground(new Color(69, 104, 159));
+        table.getTableHeader().setFont(new Font("Arial", Font.BOLD, 12));
         table.setFont(new Font("Arial", Font.PLAIN, 16));
         table.setForeground(new Color(69, 104, 159));
-        table.setBorder(new LineBorder(new Color(69, 104, 159), 3));
+        table.setBorder(new LineBorder(new Color(69, 104, 159), 2));
         table.setGridColor(new Color(69, 104, 159));
+        scrollPane.setBorder(new LineBorder(new Color(69, 104, 159), 2));
         
+        header.setFont(new Font("Arial", Font.BOLD, 28));
+        date.setFont(new Font("Arial", Font.BOLD, 28));
+        date.setForeground(new Color(69, 104, 159));
+        top.setBorder(BorderFactory.createEmptyBorder(10,0,10,0));
+        top.add(header);
+        top.add(date);
+        top.setBackground(Color.white);
+        
+        empty1.setBackground(Color.white);
+        empty2.setBackground(Color.white);
+        empty3.setBackground(Color.white);
+        empty1.setPreferredSize(new Dimension(20,0));
+        empty2.setPreferredSize(new Dimension(20,0));
+        empty3.setPreferredSize(new Dimension(0,30));
+        
+        frame.add(top, BorderLayout.NORTH);
         frame.add(scrollPane);
-        frame.setSize(800,300);
+        frame.add(empty1, BorderLayout.WEST);
+        frame.add(empty2, BorderLayout.EAST);
+        frame.add(empty3, BorderLayout.SOUTH);
+        frame.setSize(950,500);
         frame.setVisible(true);
     }
     
@@ -69,4 +108,5 @@ public class ViewHistory {
         });
     }
 }
+
 
