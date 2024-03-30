6e6d7f4288de7851ab7df975d28ee4828931b42a
zedl3all <sanguanwongtanapat@gmail.com>
2024-03-30 19:44:33 +0700

    Fix parameter

--- src/UI/View/CircleProfile.java
+++ src/UI/View/CircleProfile.java
@@ -42,8 +42,8 @@ public class CircleProfile extends JPanel {
         return im1;
     }
     //Overload
-    public Image LoadImage(Image im1){
-        i1 = new ImageIcon(im1);
+    public Image LoadImage(Image img){
+        i1 = new ImageIcon(img);
         im1 = i1.getImage();
         return im1;
     }
