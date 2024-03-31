bc2d5c65b693e70aad00f42acda9b666a46baa23
zedl3all <sanguanwongtanapat@gmail.com>
2024-03-31 22:45:44 +0700

    Edit abstract and interface

--- src/My_sql/Connectable.java
+++ src/My_sql/Connectable.java
@@ -1,6 +1,13 @@
 package My_sql;
 
+import java.sql.Connection;
+
 public interface Connectable {
     public abstract void connect();
     public abstract void disconnect();
+    public void set_Schema(String schema);
+    public String get_Schema();
+    public void set_Table(String table);
+    public String get_Table();
+    public Connection get_Connection();
 }
--- src/My_sql/ProjectData/ProjectData.java
+++ src/My_sql/ProjectData/ProjectData.java
@@ -3,4 +3,6 @@ package My_sql.ProjectData;
 public sealed abstract class ProjectData permits DoProjectData{
     
     public abstract void Createtemplate(String username, String projectname);
+    public abstract void rename_schema(String user, String oldSchema, String newSchema);
+    public abstract void delete_project(String username, String projectname);
 }
