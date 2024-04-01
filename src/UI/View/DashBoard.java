198066b6eb592fc6fb84e02ce39255a3782072dd
SAKURA <141387173+Stacla3@users.noreply.github.com>
2024-04-01 22:44:24 +0700

    Fix DashBoard UI 100%

--- src/UI/View/DashBoard.java
+++ src/UI/View/DashBoard.java
@@ -8,7 +8,7 @@ import javax.swing.border.LineBorder;
 public class DashBoard extends JPanel{
     private JFrame mainf;
     private JPanel pmain, psubmain, pnorth, pcenter;
-    private JPanel pempty1, pempty2;
+    private JPanel pempty1, pempty2, pempty3;
     private JLabel jdashboard;
     private SubDashBoard_1 sdb1;
     private SubDashBoard_2 sdb2;
@@ -17,9 +17,9 @@ public class DashBoard extends JPanel{
     private Mosttypegraph mtg;
     private TopProductGraph tpg1;
     public DashBoard(){
-        icg1 = new IncomeCompareGraph();
-        mtg = new Mosttypegraph();
-        tpg1 = new TopProductGraph();
+        //icg1 = new IncomeCompareGraph();
+        //mtg = new Mosttypegraph();
+        //tpg1 = new TopProductGraph();
         sdb1 = new SubDashBoard_1();
         sdb2 = new SubDashBoard_2();
         sdb3 = new SubDashBoard_3();
@@ -30,6 +30,7 @@ public class DashBoard extends JPanel{
         pcenter = new JPanel();
         pempty1 = new JPanel();
         pempty2 = new JPanel();
+        pempty3 = new JPanel();
         jdashboard = new JLabel("DASH BOARD");
         jdashboard.setHorizontalAlignment(SwingConstants.CENTER);
         
@@ -41,6 +42,7 @@ public class DashBoard extends JPanel{
         pcenter.setLayout(new BorderLayout());
         pempty1.setLayout(new GridLayout(1, 3));
         pempty2.setLayout(new GridLayout(1, 3));
+        pempty3.setLayout(new BorderLayout());
         
         //SetBackGround
         this.setBackground(Color.WHITE);
@@ -51,11 +53,12 @@ public class DashBoard extends JPanel{
         psubmain.setBackground(Color.WHITE);
         pempty1.setBackground(Color.WHITE);
         pempty2.setBackground(Color.WHITE);
+        pempty3.setBackground(Color.WHITE);
         pnorth.setBackground(Color.WHITE);
         pcenter.setBackground(Color.WHITE);
         
         //SetFont
-        jdashboard.setFont(new Font("Aria", Font.BOLD, 40));
+        jdashboard.setFont(new Font("Aria", Font.BOLD, 50));
         jdashboard.setForeground(new Color(69, 104, 159));
         
         //Pmain
@@ -63,18 +66,22 @@ public class DashBoard extends JPanel{
         pmain.add(pcenter);
         
         //Pcenter
-        pcenter.add(pempty1);
-        pempty1.add(icg1);
-        pempty1.add(mtg);
-        pempty1.add(tpg1);
+        pcenter.add(pempty1, BorderLayout.SOUTH);
+        //pempty1.add(icg1);
+        //pempty1.add(mtg);
+        //pempty1.add(tpg1);
         
-        pempty2.setPreferredSize(new Dimension(200, 150));
-        pcenter.add(pempty2, BorderLayout.SOUTH); 
+        pempty1.setPreferredSize(new Dimension(200, 180));
+        pcenter.add(pempty2);
         pempty2.add(sdb3);
         pempty2.add(sdb1);
         pempty2.add(sdb2);
         
+        //pempty3.setPreferredSize(new Dimension(200, 100));
+        //pcenter.add(pempty3, BorderLayout.NORTH);
+        
         //Pnorth
+        pnorth.setPreferredSize(new Dimension(200, 170));
         pnorth.add(jdashboard);
             
         //JFrame
@@ -198,6 +205,6 @@ public class DashBoard extends JPanel{
     public void setTpg1(TopProductGraph tpg1) {
         this.tpg1 = tpg1;
     }
-    
+
     
 }
