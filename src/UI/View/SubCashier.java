c0cf7514ec57e7f987cc2cc6ed96819bb1a39f56
SAKURA <141387173+Stacla3@users.noreply.github.com>
2024-04-01 13:26:10 +0700

    Add amount 100% and SubCashier 100% UI

--- src/UI/View/SubCashier.java
+++ src/UI/View/SubCashier.java
@@ -2,26 +2,32 @@
 package UI.View;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
-public class SubCashier extends JPanel {
-
+public class SubCashier extends JPanel implements ActionListener{
     private JFrame mainf;
-    private JPanel psub, psubnorth, psubcenter, psubsouth, pImage, allsubsouth;
+    private JPanel psub, psubnorth, psubcenter, psubsouth, pImage, btnPanel;
     private JPanel pempty1, pempty2, pempty3, pempty4, pempty5, pempty6;
-    private JLabel Jproduct, Jnameproduct, Jprice, Jnumprice;
+    private JLabel Jproduct, Jnameproduct, Jtype, Jnametype, Jamount, Jnumamount, Jprice, Jnumprice;
     private JLabel Jempty1, Jempty2, Jempty3, Jempty4, Jempty5, Jempty6;
-    private JButton bview;
+    private JButton plus, minus;
     private ImageIcon i1, i2;
 
     public SubCashier() {
+
         //Create
         i1 = new ImageIcon("src/UI/Image/eweweewew.jpg");
         i2 = new ImageIcon(i1.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH));
         mainf = new JFrame("SubProduct");
         Jnameproduct = new JLabel("......");
         Jproduct = new JLabel("Product Name : "+Jnameproduct.getText());
+        Jnametype = new JLabel("......");
+        Jtype = new JLabel("Type : "+Jnametype.getText());
+        Jnumamount = new JLabel("......");
+        Jamount = new JLabel("Amount : "+Jnumamount.getText());
         Jnumprice = new JLabel("......");
         Jprice = new JLabel("Price : "+Jnumprice.getText());
         Jempty1 = new JLabel();
@@ -40,8 +46,9 @@ public class SubCashier extends JPanel {
         pempty4 = new JPanel();
         pempty5 = new JPanel();
         pempty6 = new JPanel();
-        allsubsouth = new JPanel();
-        bview = new JButton("ADD");
+        btnPanel = new JPanel();
+        plus = new JButton("+");
+        minus = new JButton("-");
 
         //Psubnorth
         psubnorth.setLayout(new GridLayout(2, 1));
@@ -53,22 +60,36 @@ public class SubCashier extends JPanel {
         pempty2.add(Jproduct, BorderLayout.CENTER);
 
         //Psubsouth
-        psubsouth.setLayout(new BorderLayout());
-        psubsouth.add(allsubsouth);
-        psubsouth.setBorder(BorderFactory.createEmptyBorder(0,0,20,0));
-        allsubsouth.setLayout(new BorderLayout());
-        allsubsouth.add(pempty3, BorderLayout.NORTH);
-        allsubsouth.add(pempty5);
-        pempty3.add(Jprice);
-        pempty5.add(bview);
-        pempty5.setBorder(BorderFactory.createEmptyBorder(10,0,0,0));
+        psubsouth.setLayout(new GridLayout(4, 4));
+        psubsouth.add(pempty3);
+        psubsouth.add(pempty4);
+        psubsouth.add(Jempty1);
+        psubsouth.add(Jempty2);
+        psubsouth.add(pempty5);
+        psubsouth.add(pempty6);
+        pempty3.setLayout(new BorderLayout());
+        pempty4.setLayout(new BorderLayout());
+        pempty5.setLayout(new BorderLayout());
+        pempty6.setLayout(new BorderLayout());
+        pempty3.add(Jempty4, BorderLayout.WEST);
+        pempty3.add(Jtype);
+        pempty4.add(Jamount);
+        pempty5.add(Jempty5, BorderLayout.WEST);
+        pempty5.add(Jprice);
+        pempty6.add(Jempty6, BorderLayout.EAST);
+        plus.setFocusPainted(false);
+        minus.setFocusPainted(false);
+        btnPanel.add(plus);
+        btnPanel.add(minus);
+        btnPanel.setBackground(new Color(69, 104, 159));
+        pempty6.add(btnPanel);
         
         //SetBackground
         psubnorth.setBackground(new Color(69, 104, 159));
         psubcenter.setBackground(new Color(69, 104, 159));
         psubsouth.setBackground(new Color(69, 104, 159));
         pImage.setBackground(Color.WHITE);
-        //pImage.setPreferredSize(new Dimension(160,120));
+        pImage.setPreferredSize(new Dimension(160,120));
         pempty1.setBackground(new Color(69, 104, 159));
         pempty2.setBackground(new Color(69, 104, 159));
         pempty3.setBackground(new Color(69, 104, 159));
@@ -81,13 +102,24 @@ public class SubCashier extends JPanel {
         Jnameproduct.setForeground(Color.WHITE);
         Jproduct.setFont(new Font("Aria", Font.BOLD, 16));
         Jproduct.setForeground(Color.WHITE);
+        Jnametype.setFont(new Font("Aria", Font.PLAIN, 13));
+        Jnametype.setForeground(Color.WHITE);
+        Jtype.setFont(new Font("Aria", Font.BOLD, 14));
+        Jtype.setForeground(Color.WHITE);
+        Jnumamount.setFont(new Font("Aria", Font.PLAIN, 13));
+        Jnumamount.setForeground(Color.WHITE);
+        Jamount.setFont(new Font("Aria", Font.BOLD, 14));
+        Jamount.setForeground(Color.WHITE);
         Jnumprice.setFont(new Font("Aria", Font.PLAIN, 13));
         Jnumprice.setForeground(Color.WHITE);
         Jprice.setFont(new Font("Aria", Font.BOLD, 14));
         Jprice.setForeground(Color.WHITE);
-        bview.setFont(new Font("Aria", Font.BOLD, 13));
-        bview.setForeground(new Color(69, 104, 159));
-        bview.setBackground(Color.WHITE);
+        plus.setFont(new Font("Aria", Font.BOLD, 13));
+        plus.setForeground(new Color(69, 104, 159));
+        plus.setBackground(Color.WHITE);
+        minus.setFont(new Font("Aria", Font.BOLD, 13));
+        minus.setForeground(new Color(69, 104, 159));
+        minus.setBackground(Color.WHITE);
         
         //Psubcenter
         psubcenter.setLayout(new BorderLayout());
@@ -105,5 +137,26 @@ public class SubCashier extends JPanel {
         this.setBackground(new Color(69, 104, 159));
         this.setBorder(new LineBorder(new Color(101, 113, 132), 5));
         this.setSize(300, 400);
+    }
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        SwingUtilities.invokeLater(() -> {
+            new SubCashier();
+        });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(plus)){
+            new AddAmount();
+        }
+        else if(e.getSource().equals(minus)){
+            new AddAmount();
+        }
     }
 }
