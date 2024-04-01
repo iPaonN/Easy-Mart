5933b6716ff37a158120622bce7c5957140244de
Panot Liangpiboon <138891084+iPaonN@users.noreply.github.com>
2024-04-01 14:10:51 +0700

    update popeditprofile
    now can't rename username due to minimize the risk of incorrect data

--- src/UI/Controller/PopEditProfile.java
+++ src/UI/Controller/PopEditProfile.java
@@ -24,6 +24,8 @@ public class PopEditProfile implements ActionListener, MouseListener{
         main = new EditProfile();
         this.username = username;
         this.mainpage = mainpage;
+        main.getUsernameField().setText(manager.Get_User(this.username).getUser_name());
+        main.getUsernameField().setEditable(false);
 //        Image pic = manager.GetProfileImage(this.username);
 //        main.getProfileIcon().LoadImage(manager.GetProfileImage(String.valueOf(pic)));
         
@@ -35,18 +37,18 @@ public class PopEditProfile implements ActionListener, MouseListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().equals(main.getSaveButton())){
-            if (main.getUsernameField().getText().equals("null")){
-                JOptionPane.showMessageDialog(null, "Don't set null to username");
-                main.getUsernameField().setText("");
-            }
-            else if (main.getUsernameField().getText().equals("")){
-                JOptionPane.showMessageDialog(null, "Input your username");
-                main.getUsernameField().setText("");
-            }
-            else if (this.checkPJName(main.getUsernameField().getText()) == false){
-                JOptionPane.showMessageDialog(null, "Username must have only letter or digit");
-                main.getUsernameField().setText("");
-            }
+//            if (main.getUsernameField().getText().equals("null")){
+//                JOptionPane.showMessageDialog(null, "Don't set null to username");
+//                main.getUsernameField().setText("");
+//            }
+//            else if (main.getUsernameField().getText().equals("")){
+//                JOptionPane.showMessageDialog(null, "Input your username");
+//                main.getUsernameField().setText("");
+//            }
+//            else if (this.checkPJName(main.getUsernameField().getText()) == false){
+//                JOptionPane.showMessageDialog(null, "Username must have only letter or digit");
+//                main.getUsernameField().setText("");
+//            }
             
             if (main.getFirstNameField().getText().equals("null")){
                 JOptionPane.showMessageDialog(null, "Don't set null to firstname");
@@ -79,24 +81,24 @@ public class PopEditProfile implements ActionListener, MouseListener{
                 manager.ChangeFirstname(manager.Get_User(this.username).getFirst_name(), main.getFirstNameField().getText());
                 manager.ChangeLastname(manager.Get_User(this.username).getLast_name(), main.getLastNameField().getText());
                 
-                DPD = new DoProjectData();
-                String oldusername = this.username;
-                String newusername = main.getUsernameField().getText();
-                
-                ArrayList<String> allPJ = manager.GetProjectList(oldusername);
-                for(String i : allPJ){
-                    System.out.println(i);
-                    String []Pjname = i.split("_");
-                    System.out.println(Pjname[0]);
-                    System.out.println(Pjname[1]);
-                    DPD.rename_schema_for_editprofile(newusername, oldusername+"_"+Pjname[1], newusername+"_"+Pjname[1], Pjname[1]);
-                    manager.RenameProject(oldusername, oldusername+"_"+Pjname[1], newusername+"_"+Pjname[1]);
-                }
-                
-                manager.ChangeUsername(this.username, main.getUsernameField().getText());
-                
-                
-                System.out.println(manager.Get_User(this.username).getFirst_name());
+//                DPD = new DoProjectData();
+//                String oldusername = this.username;
+//                String newusername = main.getUsernameField().getText();
+//                
+//                ArrayList<String> allPJ = manager.GetProjectList(oldusername);
+//                for(String i : allPJ){
+//                    System.out.println(i);
+//                    String []Pjname = i.split("_");
+//                    System.out.println(Pjname[0]);
+//                    System.out.println(Pjname[1]);
+//                    DPD.rename_schema_for_editprofile(newusername, oldusername+"_"+Pjname[1], newusername+"_"+Pjname[1], Pjname[1]);
+//                    manager.RenameProject(oldusername, oldusername+"_"+Pjname[1], newusername+"_"+Pjname[1]);
+//                }
+//                
+//                manager.ChangeUsername(this.username, main.getUsernameField().getText());
+//                
+//                
+//                System.out.println(manager.Get_User(this.username).getFirst_name());
                 this.mainpage.getFr().dispose();
                 new MainPageController(main.getUsernameField().getText());
                 this.main.getFr().dispose();
