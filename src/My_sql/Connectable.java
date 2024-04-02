7a02f8763c085092efcb65948e9cc8bc64b84742
zedl3all <sanguanwongtanapat@gmail.com>
2024-04-02 15:21:35 +0700

    Add abstract

--- src/My_sql/Connectable.java
+++ src/My_sql/Connectable.java
@@ -5,9 +5,9 @@ import java.sql.Connection;
 public interface Connectable {
     public abstract void connect();
     public abstract void disconnect();
-    public void set_Schema(String schema);
-    public String get_Schema();
-    public void set_Table(String table);
-    public String get_Table();
-    public Connection get_Connection();
+    public abstract void set_Schema(String schema);
+    public abstract String get_Schema();
+    public abstract void set_Table(String table);
+    public abstract String get_Table();
+    public abstract Connection get_Connection();
 }
